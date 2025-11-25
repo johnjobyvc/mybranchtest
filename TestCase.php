27327ed7bda9bd618b433edb8ba85e266147a101
john <john.joby@anotete.co.jp>
2025-11-25 19:03:37 +0900

    my3rdb branch 4th branch 5th barnch gose

--- TestCase.php
+++ TestCase.php
@@ -1,5 +1,5 @@
 <?php
-*************thrird brach 4th BANCH GOSE ***********************
+*************thrird brach 4th BANCH GOSE 5th branch gose ***********************
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
