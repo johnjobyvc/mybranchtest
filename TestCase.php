d212b0c41072ea07b217a2cfdeca72a74d9f4c96
john <john.joby@anotete.co.jp>
2025-11-25 19:01:31 +0900

    thirdbranch4thbranchgose

--- TestCase.php
+++ TestCase.php
@@ -1,5 +1,5 @@
 <?php
-*************thrird brach after merage***********************
+*************thrird brach 4th BANCH GOSE ***********************
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
