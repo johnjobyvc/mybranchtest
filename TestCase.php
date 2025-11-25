7f28aa944991d64962cea4e32b4b267b7ce70e7d
john <john.joby@anotete.co.jp>
2025-11-25 16:47:53 +0900

    main branch commint

---
+++ TestCase.php
@@ -0,0 +1,10 @@
+<?php
+#THIS IS FROM MAIN BRANCH
+namespace Tests;
+
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    use CreatesApplication;
+}
